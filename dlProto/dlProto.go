d4be9440b645eb46b747faa6fff2a4c15283448c
Xyzyx101 <Xyzyx101@notMyRealEmail.net>
2013-05-10 05:03:25 -0400

    Web server now uses config file to set directories

--- dlProto/dlProto.go
+++ dlProto/dlProto.go
@@ -1,45 +1,39 @@
 package main
 
 import (
+	"code.google.com/p/goconf/conf"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"code.google.com/p/goconf/conf"
 )
 
-var fileLocation = "/home/andrew/code/tuig/files/"
+type configValues struct {
+	host      string
+	port      string
+	uploadDir string
+	templates string
+}
+var config configValues
 
-var indexPage = template.Must(template.ParseFiles(
-	"templates/_base.html",
-	"templates/index.html",
-))
-
-var filesPage = template.Must(template.ParseFiles(
-	"templates/_base.html",
-	"templates/files.html",
-))
-
-var uploadPage = template.Must(template.ParseFiles(
-	"templates/_base.html",
-	"templates/upload.html",
-))
-
-var thanksPage = template.Must(template.ParseFiles(
-	"templates/_base.html",
-	"templates/thanks.html",
-))
+type templates struct {
+	index  *template.Template
+	upload *template.Template
+	files  *template.Template
+	thanks *template.Template
+}
+var tmpl templates
 
 func index(w http.ResponseWriter, req *http.Request) {
-	if err := indexPage.Execute(w, nil); err != nil {
+	if err := tmpl.index.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func filesHandler(w http.ResponseWriter, req *http.Request) {
 	files := make([]os.FileInfo, 100)
-	files, err := ioutil.ReadDir(fileLocation)
+	files, err := ioutil.ReadDir(config.uploadDir)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -47,22 +41,23 @@ func filesHandler(w http.ResponseWriter, req *http.Request) {
 	type FileList struct {
 		Files []os.FileInfo
 	}
-	fileList := FileList{Files:files}
-	if err := filesPage.Execute(w, fileList); err != nil {
+	fileList := FileList{Files: files}
+	if err := tmpl.files.Execute(w, fileList); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-type ServeFileHandler struct {}
+type ServeFileHandler struct{}
+
 func (fh ServeFileHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	fileName := fileLocation + req.URL.Path
-	
+	fileName := config.uploadDir + "/" + req.URL.Path
+
 	fileBuffer, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		fmt.Println("Error reading file")
 		http.Error(w, err.Error(), http.StatusNotFound)
 	}
-	
+
 	w.Header().Set("Content-Type", "application/octet-stream")
 
 	if _, err := w.Write(fileBuffer); err != nil {
@@ -71,53 +66,85 @@ func (fh ServeFileHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 }
 
 func upload(w http.ResponseWriter, req *http.Request) {
-	if err :=uploadPage.Execute(w, nil); err != nil {
+	if err := tmpl.upload.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func thanks(w http.ResponseWriter, req *http.Request) {
-	if err :=thanksPage.Execute(w, nil); err != nil {
+	if err := tmpl.thanks.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-type configValues struct {
-	host string
-	port string
-}
-
 func parseConfigFile(file string) (configValues, error) {
-	var config configValues
 
-	c, err := conf.ReadConfigFile(file); 
+	c, err := conf.ReadConfigFile(file)
 	if err != nil {
 		return config, err
 	}
-	
+
 	config.host, err = c.GetString("server", "host")
 	if err != nil {
 		return config, err
 	}
-	
+
 	config.port, err = c.GetString("server", "port")
 	if err != nil {
 		return config, err
 	}
-		
+
+	config.uploadDir, err = c.GetString("directories", "uploadDir")
+	if err != nil {
+		return config, err
+	}
+
+	config.templates, err = c.GetString("directories", "templates")
+	if err != nil {
+		return config, err
+	}
+
 	return config, err
 }
 
+func parseTemplates() error {
+	tmpl.index = template.Must(template.ParseFiles(
+		"templates/_base.html",
+		"templates/index.html",
+	))
+
+	tmpl.files = template.Must(template.ParseFiles(
+		"templates/_base.html",
+		"templates/files.html",
+	))
+
+	tmpl.upload = template.Must(template.ParseFiles(
+		"templates/_base.html",
+		"templates/upload.html",
+	))
+
+	tmpl.thanks = template.Must(template.ParseFiles(
+		"templates/_base.html",
+		"templates/thanks.html",
+	))
+	return nil
+}
+
 func main() {
-	
+
 	config, err := parseConfigFile("webserver.conf")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 	
+	if err := parseTemplates(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
 	http.HandleFunc("/", index)
-	
+
 	http.HandleFunc("/files/", filesHandler)
 
 	var getFile ServeFileHandler
@@ -128,7 +155,7 @@ func main() {
 	http.HandleFunc("/thanks/", thanks)
 
 	fmt.Printf("Webserver started %s:%s\n", config.host, config.port)
-	if err := http.ListenAndServe(config.host + ":" + config.port, nil); err != nil {
+	if err := http.ListenAndServe(config.host+":"+config.port, nil); err != nil {
 		panic(err)
 	}
 }

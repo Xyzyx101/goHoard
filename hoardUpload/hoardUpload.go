608c655152e217aa7355a96ea7de14111122b0c4
Xyzyx101 <Xyzyx101@notMyRealEmail.net>
2013-05-11 13:17:49 -0400

    Changed upload service to use the config package

--- hoardUpload/hoardUpload.go
+++ hoardUpload/hoardUpload.go
@@ -1,24 +1,15 @@
 package main
 
 import (
-	"code.google.com/p/goconf/conf"
 	"fmt"
+	"github.com/Xyzyx101/goHoard/config"
 	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
 
-type configValues struct {
-	host          string
-	port          string
-	uploadDir     string
-	templates     string
-	webserverHost string
-	webserverPort string
-}
-
-var config configValues
+var conf config.Config
 
 type templates struct {
 	upload *template.Template
@@ -31,7 +22,7 @@ func postFile(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	fileName := config.uploadDir + "/" + fileHeader.Filename
+	fileName := conf["dir"]["upload"] + "/" + fileHeader.Filename
 
 	var filePerm os.FileMode = 0664
 
@@ -44,59 +35,21 @@ func postFile(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
-	thanksPage := "http://" + config.webserverHost + ":" + config.webserverPort + "/thanks"
+	thanksPage := "http://" + conf["webserver"]["host"] + ":" + conf["webserver"]["port"] + "/thanks"
 	http.Redirect(w, req, thanksPage, http.StatusSeeOther)
-}
-
-func parseConfigFile(file string) (configValues, error) {
-
-	c, err := conf.ReadConfigFile(file)
-	if err != nil {
-		return config, err
-	}
-
-	config.host, err = c.GetString("uploadserver", "host")
-	if err != nil {
-		return config, err
-	}
-
-	config.port, err = c.GetString("uploadserver", "port")
-	if err != nil {
-		return config, err
-	}
-
-	config.uploadDir, err = c.GetString("directories", "uploadDir")
-	if err != nil {
-		return config, err
-	}
-
-	config.templates, err = c.GetString("directories", "templates")
-	if err != nil {
-		return config, err
-	}
-	config.webserverHost, err = c.GetString("webserver", "host")
-	if err != nil {
-		return config, err
-	}
-
-	config.webserverPort, err = c.GetString("webserver", "port")
-	if err != nil {
-		return config, err
-	}
-
-	return config, err
 }
 
 func parseTemplates() error {
 	tmpl.upload = template.Must(template.ParseFiles(
-		config.templates+"/_base.html",
-		config.templates+"/upload.html",
+		conf["dir"]["template"]+"/_base.html",
+		conf["dir"]["template"]+"/upload.html",
 	))
 	return nil
 }
 
 func main() {
-	config, err := parseConfigFile("../server.conf")
+	var err error
+	conf, err = config.ParseFile("../server.conf")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -109,8 +62,10 @@ func main() {
 
 	http.HandleFunc("/postFile", postFile)
 
-	fmt.Printf("Upload server started %s:%s\n", config.host, config.port)
-	if err := http.ListenAndServe(config.host+":"+config.port, nil); err != nil {
+	host := conf["uploadserver"]["host"]
+	port := conf["uploadserver"]["port"]
+	fmt.Printf("Upload server started %s:%s\n", host, port)
+	if err := http.ListenAndServe(host+":"+port, nil); err != nil {
 		panic(err)
 	}
 
